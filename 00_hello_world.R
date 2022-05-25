d80e593ae7f6e239fd50018d82fd88eb81a5ac8a
fhernanb <fhernanb@unal.edu.co>
2022-05-24 19:13:28 -0500

    nuevo

---
+++ 00_hello_world.R
@@ -0,0 +1,14 @@
+# En este script vamos a replicar los ejemplos mostrados por David Malan
+# en el video https://youtu.be/TJKnZ784bSI
+# para el curso CS50
+
+# Minuto = 30:20
+print("Hello, world")
+
+# Minuto = 43:10
+readline("What's your name? ")
+print("Hello, Freddy")
+
+# Minuto = 45:41
+name <- readline("What's your name? ")
+print(paste("Hello,", name))
